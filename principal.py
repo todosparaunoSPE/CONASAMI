7b8ee8b74bfdacc721f559ea94810e0b881fc526
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-22 09:38:57 -0600

    Update principal.py

--- principal.py
+++ principal.py
@@ -7,6 +7,23 @@ Created on Fri Nov 22 09:30:06 2024
 
 import streamlit as st
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 # Configuración de la página de Streamlit
 st.set_page_config(page_title="Convocatoria - Coordinación para el Análisis de Economía Laboral en CONASAMI", layout="wide")
 
